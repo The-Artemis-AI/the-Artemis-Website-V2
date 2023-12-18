7b0532fdb1a2cc4ca6c3af19beeafdebcfe3f2cb
Gatete Angelo Christian <ishgatetechristian@gmail.com>
2023-12-18 17:22:22 +0200

    Fix: Deployment

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"/the-Artemis-Website-V2/"
+  // base:"/the-Artemis-Website-V2/"
 })
