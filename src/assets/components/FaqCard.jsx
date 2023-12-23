87763923b3c526f95dcf5afe481fbbd5cc1b8ca3
Gatete Angelo Christian <ishgatetechristian@gmail.com>
2023-12-23 21:29:38 +0200

    Fix: Add name of site

--- src/assets/components/FaqCard.jsx
+++ src/assets/components/FaqCard.jsx
@@ -6,14 +6,14 @@ function FaqCard() {
 
     const [showResponse, setShowResponse] = useState(false)
   return (
-    <div onClick={()=>setShowResponse(!showResponse)} className='w-full  p-5 my-2 cursor-pointer' style={{backgroundColor:showResponse?"#F0FFB6": "white", border:showResponse?"none":"1px solid rgba(0,0,0,0.1)"}}>
-    <div className='flex flex-row justify-between items-center'>
-    <p className='font-semibold text-sm'>Lore ipsum dolor sit amet consecteur. In eu aliquet arci lobortis aenaes ?</p>
+    <div onClick={()=>setShowResponse(!showResponse)} className='w-full  p-5 my-2 cursor-pointer transition-all' style={{backgroundColor:showResponse?"#F0FFB6": "white", border:showResponse?"none":"1px solid rgba(0,0,0,0.1)"}}>
+    <div className='flex flex-row justify-between items-center transition-all'>
+    <p className='font-semibold text-sm phone:text-[1em]'>Lore ipsum dolor sit amet consecteur. In eu aliquet arci lobortis aenaes ?</p>
    
    <button onClick={()=>setShowResponse(!showResponse)} className='w-[10%  flex flex-row justify-center items-center] p-1 rounded-full'> {showResponse? <FiMinus className='text-xl w-full text-yellow-600'/>:  <IoIosAdd className='text-xl w-full'/>}</button> 
     </div>
-   {showResponse && <div className='w-full delay-100 ease-in-out'>
-    <p className='text-sm w-[90%] mt-2'> Lore ipsum dolor sit amet consecteur. In eu aliquet arci lobortis aenaes Lore ipsum dolor sit amet consecteur. In eu aliquet arci lobortis aenaes
+   {showResponse && <div className='w-full delay-100 ease-in-out transition-all'>
+    <p className='text-sm phone:text-[1em] w-[90%] mt-2'> Lore ipsum dolor sit amet consecteur. In eu aliquet arci lobortis aenaes Lore ipsum dolor sit amet consecteur. In eu aliquet arci lobortis aenaes
     Lore ipsum dolor sit amet consecteur. In eu aliquet arci lobortis aenaes Lore ipsum dolor sit amet consecteur. In eu aliquet arci lobortis aenaes</p>
     </div>}
     </div>
--- src/assets/components/HomeSections/FAQSection.jsx
+++ src/assets/components/HomeSections/FAQSection.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import FaqCard from '../FaqCard'
 function FAQSection() {
   return (
-    <div className='w-full  h-auto p-10 px-20  bg-white flex flex-col justify-aroun items-center'>
+    <div className='w-full h-auto p-10 px-5 phone:px-10 bigphone:px-20  bg-white flex flex-col justify-aroun items-center'>
     <div className="faq-section">
     <h3 className='text-4xl text-center my-2 mb-5'>FAQ</h3>
     <p className='mb-5 w-[70%] text-center mx-auto'>Frequently Asked Questions. Find some common questions below about Artemis AI</p>
