ff540252df768ca895a38ba63a4ad49c98177c2b
Gatete Angelo Christian <ishgatetechristian@gmail.com>
2024-01-13 20:36:51 +0200

    Fix: Navigation

--- src/assets/components/BlogCard.jsx
+++ src/assets/components/BlogCard.jsx
@@ -4,11 +4,11 @@ import { Link } from 'react-router-dom'
 
 function BlogCard(props) {
   return (
-    <a href={"/the-Artemis-Website-V2/media/" + props.Link} className='bigphone:w-[50%] bigphone:basis-[45%] tablet:w-[30%] tablet:basis-[30%] h-[40%] m-1 mb-4 group hover:scale-105 transition-all cursor-pointer'>
+    <Link to={"/the-Artemis-Website-V2/media/" + props.Link} className='bigphone:w-[50%] bigphone:basis-[45%] tablet:w-[30%] tablet:basis-[30%] h-[40%] m-1 mb-4 group hover:scale-105 transition-all cursor-pointer'>
     <img src={props.Image} alt='image' className='w-full h-72 object-cover blur-[2px] grayscale-0 group-hover:blur-0'/>
     <p className='text-xl mt-1 font-semibold' style={{fontFamily:'Montserrat'}}>{props.title}</p>
     <p className='text-gray-400 text-sm' style={{fontFamily:'Montserrat'}}>{props.subtitle}</p>
-    </a>
+    </Link>
   )
 }
 
