12b7cebf454823facd5345870eef5c45f92402ad
Gatete Angelo Christian <ishgatetechristian@gmail.com>
2024-01-13 22:00:07 +0200

    Fix: Error page

--- src/assets/components/HomeSections/ErrorPage.jsx
+++ src/assets/components/HomeSections/ErrorPage.jsx
@@ -1,5 +1,6 @@
 // import React from 'react'
 import { IoIosArrowRoundForward } from 'react-icons/io'
+import { useHistory } from 'react-router-dom';
 import Robot from "../../images/robot-assistant-sad.webp"
 import { Link } from 'react-router-dom'
 import ContactSection from '../ContactSection'
@@ -7,6 +8,13 @@ import Footer from '../Footer'
 import Navbar from '../Navbar'
 
 function ErrorPage() {
+
+  let history = useHistory();
+
+  function handleClick() {
+    history.goBack();
+  }
+
   return (
     <div>
     <Navbar />
@@ -15,10 +23,10 @@ function ErrorPage() {
     <h3 className='text-xl bigphone:text-3xl text-center my-2'> Oh no, we can't seem to find your Page</h3>
     <p className='my-2 m-[80%] mx-auto text-xs bigphone:text-sm text-center'>Looks like our search algorith may have been underfit</p>
 
-    <a href="/the-Artemis-Website-V2/" className="flex space-x-3 p-2 px-3 rounded-full items-center self-center bg-blacky">
+    <Link to="/the-Artemis-Website-V2" ><button onClick={handleClick} className="flex space-x-3 p-2 px-3 rounded-full items-center self-center bg-blacky">
 <p className="text-xs font-bold text-white">Go Back</p>
 <IoIosArrowRoundForward className="bg-gradient-to-r from-blue-500 to-green-500 text-white rounded-full w-6 h-6 -rotate-45 self-center" />
-</a>
+</button></Link>
     </div>
     <ContactSection/>
     <Footer/>
